db3bdd01e67cafb6ea075e8a105946c057f7d638
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-22 15:39:31 -0300

    added nav bar, started custom styling

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import Display from './components/Display';
+import Footer from './components/Footer';
+import './styles/Main.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Display />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
