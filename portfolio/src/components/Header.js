0970450385ae36026eaedacba36feb0f12166d72
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-22 11:23:29 -0300

    addded app js compents for header display footer

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+
+function NavBar() {
+    return (
+      <div className="nav">
+      <Nav  />
+      </div>
+    );
+  }
+  
+  export default NavBar;
