3169d0ae5971a9504d68762bba568211871e37b1
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-23 11:53:25 -0300

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,35 @@
-function Footer() {
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import Paper from '@mui/material/Paper';
 
+function Footer() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef(null);
+
+    return (
+        <Box sx={{ pb: 7 }} ref={ref}>
+            <CssBaseline />
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction icon={<LinkedInIcon />} />
+                    <BottomNavigationAction icon={<GitHubIcon />} />
+                    <BottomNavigationAction icon={<XIcon />} />
+                </BottomNavigation>
+            </Paper>
+        </Box>
+    );
 }
 
 export default Footer;
