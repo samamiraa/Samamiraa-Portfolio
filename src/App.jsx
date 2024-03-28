d0019677b28960ced85835d8248343672296cd3b
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-28 10:10:09 -0300

    moved imgs and styling to public folder and added paths to all project img in json file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Outlet } from 'react-router-dom';
-import './styles/Main.css'
+import '../public/styles/Main.css'
 
 const darkTheme = createTheme({
   palette: {
