4f8ff7f4b40cc27777e1cab40b6d2e97cbde62cc
Samantha Allen <samanthaamiraallen@gmail.com>
2024-03-31 12:08:09 -0300

    finished resume lists

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -8,16 +7,15 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import FormGroup from '@mui/material/FormGroup';
 import Grid from '@mui/material/Grid';
-
-function generate(element) {
-    return [0, 1, 2].map((value) =>
-        React.cloneElement(element, {
-            key: value,
-        }),
-    );
-}
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import CodeIcon from '@mui/icons-material/Code';
+import CodeOffIcon from '@mui/icons-material/CodeOff';
+import StyleIcon from '@mui/icons-material/Style';
+import StorageIcon from '@mui/icons-material/Storage';
+import DataObjectIcon from '@mui/icons-material/DataObject';
 
 const Demo = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.background.paper,
@@ -31,27 +29,188 @@ function Resume() {
                 <Typography variant="h5" sx={{ color: '#90caf9' }} gutterBottom>
                     Technical Skills
                 </Typography>
-                <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
-                    <FormGroup row>
-                    </FormGroup>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
-                            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                                Icon with text
-                            </Typography>
+                <Box sx={{ flexGrow: 1, maxWidth: 752}}>
+                    <Grid container spacing={4} sx={{ justifyContent: 'center'}}>
+                        <Grid item xs={12} sm={6} md={4}>
                             <Demo>
-                                <List dense='true'>
-                                    {generate(
-                                        <ListItem>
-                                            <ListItemIcon>
-
+                                <List dense={true} sx={{textAlign: 'center'}}>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <HtmlIcon />
                                             </ListItemIcon>
                                             <ListItemText
-                                                primary="Single-line item"
-
+                                                primary='HTML'
                                             />
-                                        </ListItem>,
-                                    )}
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CssIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='CSS'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <JavascriptIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='JavaScript'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='Jquery'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='React'
+                                            />
+                                        </ListItem>
+                                </List>
+                            </Demo>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Demo>
+                                <List dense={true}>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <DataObjectIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='OOP'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <DataObjectIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='ORM'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeOffIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='MVC'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeOffIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='PWA'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeOffIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='APIs'
+                                            />
+                                        </ListItem>
+                                </List>
+                            </Demo>
+                        </Grid>
+                        
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Demo>
+                                <List dense={true}>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='Node.js'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='Express.js'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='MERN'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <CodeIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='State'
+                                            />
+                                        </ListItem>
+                                </List>
+                            </Demo>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Demo>
+                                <List dense={true}>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <StyleIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='Bootstrap'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <StyleIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='Material UI'
+                                            />
+                                        </ListItem>
+                                </List>
+                            </Demo>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Demo>
+                                <List dense={true}>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <StorageIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='mySQL'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <StorageIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='NoSQL'
+                                            />
+                                        </ListItem>
+                                        <ListItem color='textSecondary'>
+                                            <ListItemIcon sx={{ marginRight: '5px'}}>
+                                                <StorageIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary='GraphQL'
+                                            />
+                                        </ListItem>
                                 </List>
                             </Demo>
                         </Grid>
@@ -62,7 +221,7 @@ function Resume() {
                     startIcon={<FileDownloadIcon />}
                     href="/resume/resume.docx"
                     download="resume.docx"
-                    sx={{ mt: 2, mb: 4}}
+                    sx={{ mt: 2, mb: 4 }}
                 >
                     Download Resume
                 </Button>
